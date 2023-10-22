b93ffc568a6664721aeb597704ea7e6500ce4684
Sweetreams <pashasurov12345@gmail.com>
2023-10-23 03:52:36 +0700

    Экран приветсвия 40%

---
+++ app/src/main/java/com/example/zenflow/welcome_screen_start.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcome_screen_start : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen_start)
+    }
+}
