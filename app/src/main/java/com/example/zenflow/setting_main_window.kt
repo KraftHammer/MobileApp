f19e87a0eaddc92c0420e119ce046ff7fff45634
Sweetreams <pashasurov12345@gmail.com>
2023-10-24 17:20:59 +0700

    Настройки 20%

---
+++ app/src/main/java/com/example/zenflow/setting_main_window.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class setting_main_window : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_main_window)
+    }
+}
