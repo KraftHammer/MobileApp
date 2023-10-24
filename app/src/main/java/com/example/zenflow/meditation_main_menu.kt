79ebd4c45fe75a8627ca0a3b0c77f9b20314a5a6
Sweetreams <pashasurov12345@gmail.com>
2023-10-24 17:46:53 +0700

    meditation

---
+++ app/src/main/java/com/example/zenflow/meditation_main_menu.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class meditation_main_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meditation_main_menu)
+    }
+}
---
+++ app/src/main/java/com/example/zenflow/setting_frequent_questions.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class setting_frequent_questions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_frequent_questions)
+    }
+}
