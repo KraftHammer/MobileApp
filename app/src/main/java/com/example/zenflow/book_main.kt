c915d067eb13ab825e923efc8ed9ad143f04f877
Sweetreams <pashasurov12345@gmail.com>
2023-10-26 10:55:25 +0700

    literally

---
+++ app/src/main/java/com/example/zenflow/book_main.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class book_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_main)
+    }
+}
