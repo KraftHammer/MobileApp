70cad2eb4071b6ff8fcda3db15087d591e7b33ab
Sweetreams <pashasurov12345@gmail.com>
2023-10-26 22:19:00 +0700

    no image

---
+++ app/src/main/java/com/example/zenflow/BookAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.zenflow
+
+import android.content.Context
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class BookAdapter(var items: List<Book>, val context:Context) : RecyclerView.Adapter<BookAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.book_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BookAdapter.ViewHolder, position: Int) {
+        holder.title.text = items[position].Title
+        holder.teext.text = items[position].Text
+
+        var imageID = context.resources.getIdentifier(
+            items[position].Image,
+            "drawble",
+            context.packageName
+        )
+
+        holder.image.setImageResource(imageID)
+    }
+
+    override fun getItemCount(): Int {
+        return items.count()
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val image:ImageView = view.findViewById(R.id.image_book)
+        val title:TextView = view.findViewById(R.id.title_book)
+        val teext:TextView = view.findViewById(R.id.text_book)
+    }
+}
--- app/src/main/java/com/example/zenflow/book_main.kt
+++ app/src/main/java/com/example/zenflow/book_main.kt
@@ -2,10 +2,25 @@ package com.example.zenflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class book_main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_main)
+
+        val itemsList : RecyclerView = findViewById(R.id.meditation_RecycleView)
+        val item123 = arrayListOf<Book>()
+
+        item123.add(Book("qweqweqwe.png", "title", "title", "title"))
+        item123.add(Book("logo.png", "title", "title", "title"))
+        item123.add(Book("lotus.xml", "title", "title", "title"))
+        item123.add(Book("setting_foto", "title", "title", "title"))
+        item123.add(Book("qweqweqwe", "title", "title", "title"))
+        item123.add(Book("qweqweqwe", "title", "title", "title"))
+
+        itemsList.layoutManager = LinearLayoutManager(this)
+        itemsList.adapter = BookAdapter(item123, this)
     }
 }
