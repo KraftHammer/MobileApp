b41702d717c5c038e65e7620ea288395504dc5cb
Yavorskiy561 <yavorskiy_ilya@inbox.ru>
2023-10-27 11:51:31 +0700

    qe

---
+++ app/src/main/java/com/example/zenflow/basic_prepare.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class basic_prepare : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_prepare)
+    }
+}
