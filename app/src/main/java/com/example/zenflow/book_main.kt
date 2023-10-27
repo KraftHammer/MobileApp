b1f761404625a36889a801d1ad0bebd308d45bd9
Sweetreams <pashasurov12345@gmail.com>
2023-10-27 23:22:57 +0700

    Я сделал лит

--- app/src/main/java/com/example/zenflow/book_main.kt
+++ app/src/main/java/com/example/zenflow/book_main.kt
@@ -16,7 +16,7 @@ class book_main : AppCompatActivity() {
         val item123 = arrayListOf<Book>()
         val back:ImageView = findViewById(R.id.meditation_back)
 
-        item123.add(Book("qweqweqwe", "Что такое Ёга?", "Предлагаем вниманию читателя научно-популярный труд Йога Рамачараки, посвященный учению индийских йогов. Эта книга для тех кто ищет реальное Знание о реальном Мире. Рамачарака отвечает на самые сложные вопросы реинакрнации и развития Души.", "title"))
+        item123.add(Book("qweqweqwe", "Что такое Ёга?", "Предлагаем вниманию читателя научно-популярный труд Йога Рамачараки, посвященный учению индийских йогов. Эта книга для тех кто ищет реальное Знание о реальном Мире. Рамачарака отвечает на самые сложные вопросы реинакрнации и развития Души.", "https://sharlib.com/read_10961-1"))
         item123.add(Book("logo.png", "title", "title", "title"))
         item123.add(Book("lotus.xml", "title", "title", "title"))
         item123.add(Book("setting_foto", "title", "title", "title"))
