402ed4b8bb8a98f616701a71f3711a19e578e3c8
Yavorskiy561 <yavorskiy_ilya@inbox.ru>
2023-10-27 11:17:23 +0700

    qe

---
+++ app/src/main/java/com/example/zenflow/prepare_meditation.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class prepare_meditation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prepare_meditation)
+    }
+}
