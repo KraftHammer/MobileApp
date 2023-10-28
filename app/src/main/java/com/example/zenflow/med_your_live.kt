d1a86b23ba8572adc09f93eecf27e8553c5e77eb
Yavorskiy561 <yavorskiy_ilya@inbox.ru>
2023-10-28 09:26:57 +0700

    qe

---
+++ app/src/main/java/com/example/zenflow/med_your_live.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class med_your_live : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_med_your_live)
+    }
+}
---
+++ app/src/main/java/com/example/zenflow/prepare.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class prepare : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prepare)
+    }
+}
