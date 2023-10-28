4f8a1ce9029ef9c321eacb573c2aea8497901140
Yavorskiy561 <yavorskiy_ilya@inbox.ru>
2023-10-28 10:11:04 +0700

    qe

---
+++ app/src/main/java/com/example/zenflow/easy_level_meditation.kt
@@ -0,0 +1,11 @@
+package com.example.zenflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class easy_level_meditation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_easy_level_meditation)
+    }
+}
