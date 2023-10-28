d5920ebed95ce84cc4b0b178abc3a16d9c0e8518
Yavorskiy561 <yavorskiy_ilya@inbox.ru>
2023-10-28 12:12:43 +0700

    qe

--- app/src/main/java/com/example/zenflow/technic_meditation.kt
+++ app/src/main/java/com/example/zenflow/technic_meditation.kt
@@ -3,9 +3,9 @@ package com.example.zenflow
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class easy_level_meditation : AppCompatActivity() {
+class technic_meditation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_easy_level_meditation)
+        setContentView(R.layout.activity_technic_meditation)
     }
 }
