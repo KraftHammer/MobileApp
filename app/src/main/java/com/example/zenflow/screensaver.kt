cd10d8a41736cb1ea7a9567379a32120c22c8ff2
Yavorskiy561 <yavorskiy_ilya@inbox.ru>
2023-10-30 22:34:18 +0700

    qe

--- app/src/main/java/com/example/zenflow/screensaver.kt
+++ app/src/main/java/com/example/zenflow/screensaver.kt
@@ -17,19 +17,25 @@ class screensaver : AppCompatActivity() {
         var upDown = loadAnimation(this, R.anim.anim_translate)
         var downUp = loadAnimation(this, R.anim.anim_translate1)
         var scaleUp = loadAnimation(this, R.anim.anim_scale)
+
         var linearLayout2 = findViewById<LinearLayout>(R.id.linearLayout2)
         var textView2: TextView = findViewById<TextView>(R.id.textView3)
+
         var line_3 = findViewById<LinearLayout>(R.id.line_3)
         var line_4 = findViewById<LinearLayout>(R.id.line_4)
 
-        line_3.startAnimation(scaleUp)
-        line_4.startAnimation(scaleUp)
-        linearLayout2.startAnimation(upDown)
-        textView2.startAnimation(downUp)
+
 
         Handler().postDelayed({
+
+            line_3.startAnimation(scaleUp)
+            line_4.startAnimation(scaleUp)
+            linearLayout2.startAnimation(upDown)
+            textView2.startAnimation(downUp)
+
             val intent = Intent(this, welcome_screen_with_button1::class.java)
             startActivity(intent)
+
         }, 3000)
     }
 }
